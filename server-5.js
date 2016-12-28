5595abe0e39a9b15814f92c6709560de21ac5eb3
Chris Wei <yunzhi.wei@live.com>
2016-12-28 09:10:12 +0800

    duplicate a copy of server.js

---
+++ server-5.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+
+// routers
+
+// declare all routers together
+// var routes = require('routes');
+// routes(app);
+
+// declare each router one by one
+app.use('/dishes', require('./routes/dishes'));
+app.use('/promotions', require('./routes/promotions'));
+app.use('/leaders', require('./routes/leaders'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, hostname, function(){
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
